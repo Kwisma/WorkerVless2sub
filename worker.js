b94ef7034a21217d161a951ab84de7e07e521ce4
sameu <77215384+Kwisma@users.noreply.github.com>
2025-02-10 11:24:41 +0800

    Update and rename _worker.js to worker.js

--- worker.js
+++ worker.js
@@ -529,6 +529,7 @@ export default {
 
 		if (env.PROXYIP) proxyIPs = await 整理(env.PROXYIP);
 		//console.log(proxyIPs);
+		await sendMessage(`#获取订阅 ${FileName}`, request.headers.get('CF-Connecting-IP'), `UA: ${userAgentHeader}</tg-spoiler>\n域名: ${url.hostname}\n<tg-spoiler>入口: ${url.pathname + url.search}</tg-spoiler>`);
 
 		if (快速订阅访问入口.length > 0 && 快速订阅访问入口.some(token => url.pathname.includes(token))) {
 			host = "null";
@@ -565,8 +566,6 @@ export default {
 				else if (uuid == "null") 空字段 = "UUID";
 				EndPS += ` 订阅器内置节点 ${空字段} 未设置！！！`;
 			}
-
-			await sendMessage(`#获取订阅 ${FileName}`, request.headers.get('CF-Connecting-IP'), `UA: ${userAgentHeader}</tg-spoiler>\n域名: ${url.hostname}\n<tg-spoiler>入口: ${url.pathname + url.search}</tg-spoiler>`);
 		} else {
 			host = url.searchParams.get('host');
 			uuid = url.searchParams.get('uuid') || url.searchParams.get('password') || url.searchParams.get('pw');
